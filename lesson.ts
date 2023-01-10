7776d1182b3428e4332746e9cea580de5b6f9725
aaronjwhowie <aaronjwhowie@gmail.com>
2023-01-10 13:34:50 -0500

    First Commit

---
+++ lesson.ts
@@ -0,0 +1,39 @@
+/*  
+--------------- Presentation Slot ---------------
+Source Material:
+https://www.youtube.com/watch?v=uTIy6MspSK0
+
+
+Wednesday 1/11/23 2:40pm Student 1 
+
+Zoom Link :
+https://redventures.zoom.us/j/95939454952?pwd=UTlkSDQ0VlR4WlhLbnF2alZ5V1JlZz09&from=addon
+
+
+Full Requirements:
+https://docs.google.com/document/d/1Q5SDjyE9c6D-EkMGC1JARXxg_qEuD3pQ12MZYCURsnE/edit#
+
+--------------- Requirements ---------------
+
+All the classes you create must contain a constructor method, 
+and 2 methods.  
+
+
+--------------- First Half ---------------
+
+
+Terms (OOP- object oriented programming)
+
+Encapsulation 
+Abstraction 
+Inheritance 
+Polymorphism 
+
+
+--------------- Second Half ---------------
+You Must Include a Github Readme file Essay 2 Paragraphs explaining these terms and
+why we would use them  -  Static, Protected, Private.
+
+
+
+*/
